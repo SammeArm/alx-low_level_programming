2d791ba99956a6704e5bcb2f01ac1db1b14b4c3e
SammeArm <sammearmosei@gmail.com>
2022-09-10 14:17:28 -0700

    task 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-* main - printing numbers from 0-9 with commas and space betwee* Description: using the main function
+* main - printing numbers from 0-9 with commas and space betwe
+* Description: using the main function
 * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
 * Return: 0
 */
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -13,10 +13,10 @@ int main(void)
 char c = 'z';
 
 while (c >= 'a')
-	{
-		putchar(c);
-		c--;
-	}
+{
+	putchar(c);
+	c--;
+}
 putchar('\n');
 return (0);
 }
