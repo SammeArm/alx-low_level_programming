66658706149ecb66346ecb7c591ff383d8eb8ae1
SammeArm <sammearmosei@gmail.com>
2022-09-14 05:06:09 -0700

    task 2-9 corrected

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
 *print_sign - determin sign of int
-*/
 *@n: number to check for sign
 *Return: 1 if positive, 0 if 0, and -1 if negative
 */
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 	
 	int mins;
 	
-	for (hrs = 0; hrs < 24; hrs++
+	for (hrs = 0; hrs < 24; hrs++)
 	{
 		for (mins = 0; mins < 60; mins++)
 		{
